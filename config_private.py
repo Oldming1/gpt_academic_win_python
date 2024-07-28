7cfef44275393b96d64195b1bf5a5f58b75985c8
oldming <86954573+Oldming1@users.noreply.github.com>
2024-07-28 22:37:48 +0800

    Update config_private.py

--- config_private.py
+++ config_private.py
@@ -2,7 +2,12 @@ API_KEY = "此处填API密钥"
 USE_PROXY = False
 proxies = None
 LLM_MODEL = "gpt-3.5-turbo"
-AVAIL_LLM_MODELS = ['gpt-4-1106-preview', 'gpt-4-turbo-preview', 'gpt-4-vision-preview', 'gpt-4o', 'gpt-4-turbo', 'gpt-4-turbo-2024-04-09', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo', 'azure-gpt-3.5', 'gpt-4', 'gpt-4-32k', 'azure-gpt-4', 'glm-4', 'glm-4v', 'glm-3-turbo', 'gemini-pro', 'chatglm3', "glm-4-0520", "glm-4-air", "glm-4-airx", "glm-4-flash",]
+AVAIL_LLM_MODELS = ["gpt-4-1106-preview", "gpt-4-turbo-preview", "gpt-4-vision-preview",
+                    "gpt-4o", "gpt-4o-mini", "gpt-4-turbo", "gpt-4-turbo-2024-04-09",
+                    "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-3.5-turbo", "azure-gpt-3.5",
+                    "gpt-4", "gpt-4-32k", "azure-gpt-4", "glm-4", "glm-4v", "glm-3-turbo",
+                    "gemini-pro", "chatglm3"
+                    ]
 API_URL_REDIRECT = {}
 DEFAULT_WORKER_NUM = 3
 THEME = "Default"
